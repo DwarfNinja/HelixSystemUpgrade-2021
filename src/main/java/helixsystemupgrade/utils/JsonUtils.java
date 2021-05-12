d245ff856dc18f766f36637369d3dbf711e86bfb
DwarfNinja <cenduroyib@gmail.com>
2021-05-13 00:40:16 +0200

    Added createJsonArray() to JsonUtils
    Changed getAllAccounts() to use JsonUtils.createJsonArray().

--- src/main/java/helixsystemupgrade/utils/JsonUtils.java
+++ src/main/java/helixsystemupgrade/utils/JsonUtils.java
@@ -2,6 +2,12 @@ package helixsystemupgrade.utils;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import helixsystemupgrade.model.Account;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 public class JsonUtils {
 
@@ -18,5 +24,15 @@ public class JsonUtils {
             e.printStackTrace();
             return "ERROR: JsonProcessingException!";
         }
+    }
+
+    public static String createJsonArray(List<Account> array) {
+        ObjectMapper objMapper = new ObjectMapper();
+        ArrayNode  jsonArray = objMapper.createArrayNode();
+
+        for (Object object : array){
+            jsonArray.addPOJO(object);
+        }
+        return jsonArray.toString();
     }
 }
